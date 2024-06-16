84e9c2ef960131555dde35912c6f1bc3f240f1a7
Ly Thai Cuong <ltc1407.5532@gmail.com>
2024-06-16 20:39:10 +0700

    Commit

--- SIMS/Shared/Models/FilterModel.cs
+++ SIMS/Shared/Models/FilterModel.cs
@@ -4,6 +4,5 @@
     {
         public string Header { get; set; }
         public List<string> Options { get; set; } = new List<string>();
-        public string SelectedValue { get; set; }
     }
 }
