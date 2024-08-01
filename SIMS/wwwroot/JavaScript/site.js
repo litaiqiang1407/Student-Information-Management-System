eeed0805a101f62a1a760fd94b453264e01ad684
popixworkspace@gmail.com <popixworkspace@gmail.com>
2024-08-01 08:41:10 -0700

    Toastcomponents

---
+++ SIMS/wwwroot/JavaScript/site.js
@@ -0,0 +1,9 @@
+﻿<script>
+    document.addEventListener('DOMContentLoaded', function () {
+    var toastElements = [].slice.call(document.querySelectorAll('.toast'));
+    toastElements.forEach(function (toastEl) {
+      var toast = new bootstrap.Toast(toastEl);
+    toast.show();
+    });
+  });
+</script>
