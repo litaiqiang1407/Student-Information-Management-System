c2dc3a277ab7655264c6c968784acc82f2737942
popivn <popixworkspace@gmail.com>
2024-08-01 08:04:17 +0700

    a

---
+++ SIMS/Shared/Services/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+namespace SIMS.Data.Entities;
+
+public interface IUserService
+{
+    Task<UserInfos> GetUserInfoAsync(string id);
+    Task<bool> AddUserInfoAsync(UserInfos userInfo, Stream fileStream, string fileName);
+    Task<bool> UpdateUserInfoAsync(string id, UserInfos userInfo, Stream fileStream, string fileName);
+}
+
+public interface IFileService
+{
+    Task<string> SaveFileAsync(IBrowserFile file);
+}
+
+public interface INotificationService
+{
+    void Notify(string message, bool isSuccess);
+}
